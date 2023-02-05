adf095ceff5eda87a07e552c348c897aa982d6d8
Isaac Blazquez <isaac.blazquez@gmail.com>
2023-02-05 12:50:03 +0100

    butacas

--- butacas.php
+++ butacas.php
@@ -8,14 +8,14 @@
     $db="teatros";
     $rs = array();
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfullyyyyyyyyyyyyy";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
 
 <?php 
@@ -111,3 +111,30 @@ try {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+}
+?>
+
+<?php
+
+
+// for($i=1;$i<=$rs0[0]['filas'];$i++){
+//     echo "i = ".$i."</br>";
+//     for($j=1;$j<=$rs0[0]['columnas'];$j++){
+//         echo "i-j = ".$i."-".$j."</br>";
+//                 $ocupada = FALSE;
+                
+//                 foreach($rs as $value){
+//                  echo "COMPARO asiento en fila: ".$value['fila']."columna: ".$value['columna']." CON i=".$i." j=".$j;
+//                   if (($value['fila']==$i)&&($value['columna']==$j)){
+//                             $ocupada = TRUE;
+//                     }
+
+//                 }
+//                     if ($ocupada) {echo "<input type='checkbox' name='butaca' value='' >";}
+//                     else{ echo "<input type='checkbox' name='butaca' value='' disabled checked='checked'>";}
+           
+//     }
+// }
+?>
