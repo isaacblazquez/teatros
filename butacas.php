7bde6f97061c8e33bd6988f2d12c18ca22ed013a
Isaac Blazquez <isaac.blazquez@gmail.com>
2023-02-05 03:58:51 +0100

    Update butacas.php

--- butacas.php
+++ butacas.php
@@ -13,7 +13,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfullyyyyyyyyyyyyy";
+  //echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
