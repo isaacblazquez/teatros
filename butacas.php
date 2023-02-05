4de7fddb924eb418e08e469655ef36616d39b111
Isaac Blazquez <isaac.blazquez@gmail.com>
2023-02-05 03:56:17 +0100

    up

---
+++ butacas.php
@@ -0,0 +1,95 @@
+<?php 
+    $id_teatro=$_GET["idTeatro"];
+    $idSesion=$_GET['idSesion'];
+?>
+<?php
+$servername = "localhost:3308";
+$username = "root";
+$password = "";
+$db="teatros";
+$rs = array();
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfullyyyyyyyyyyyyy";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+<?php
+//preparar y cegar
+$sth0 = $conn->prepare("SELECT filas,columnas FROM teatros 
+WHERE idTeatro=$id_teatro");
+$sth0->execute();
+$rs0 = $sth0->fetchAll(PDO::FETCH_ASSOC);
+ var_dump($rs0);
+
+//preparar y cegar
+$sth = $conn->prepare("SELECT e.fila,e.columna FROM teatros as t 
+LEFT JOIN sesiones as s 
+ON s.teatro=t.idTeatro
+LEFT JOIN entradas as e
+ON s.idSesion=e.idSesion
+WHERE (t.idTeatro=$id_teatro AND s.idSesion=$idSesion)");
+$sth->execute();
+$rs = $sth->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($rs);
+
+// $data = json_encode($rs);
+// echo $data;
+// $array = array();
+// foreach ($rs as $value){
+    
+     
+    
+// }
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container text-center">
+      <div class="row">
+        <form>
+            <table>
+                <tbody>
+                    <?php
+                        for($i=0;$i<$rs0[0]['filas'];$i++){
+                            echo " <tr>";
+                            for($j=0;$j<$rs0[0]['columnas'];$j++){
+                                ?>
+                                <td>
+                                    <?php 
+                                        $ocupada=false;
+                                        foreach($rs as $value){
+                                           if (($value['fila']==$i)&&($value['columna']==$j)){
+                                                $ocupada=true;
+                                           }
+                                        }
+                                        if ($ocupada) echo "<input type='checkbox' name='butaca' value='' >";
+                                        else{ echo "<input type='checkbox' name='butaca' value='' disabled checked='checked'>";}
+                                    ?> 
+                                </td> 
+                                <?php
+                            }
+                            echo " </tr>";
+                        }
+                        ?>
+                </tbody>
+            </table>
+    </form>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -34,9 +34,10 @@ $rs = $sth->fetchAll(PDO::FETCH_ASSOC);
   <body>
     <div class="container text-center">
       <div class="row">
-        <h1> Trassierra Tickes </h1>
-        <form method="post" action=<?php echo $_SERVER['PHP_SELF']; ?>>
-        <table class="table table-striped">
+       
+        <?php if (!isset($_GET['idTeatro'])){ ?>
+          <h1> Trassierra Tickes </h1>
+          <table class="table table-striped">
           <thead>
           <tr><th class="text-center">Nombre</th><th>Imagen</th></tr>
           </thead>
@@ -48,14 +49,72 @@ $rs = $sth->fetchAll(PDO::FETCH_ASSOC);
                       <h2>".$value['teatro']."</h2>
                     </td>
                     <td>
-                      <input type='image' src='img/".$value['imagen']."' style='width:500px;height:174px;' name='teatro' value='".$value['idTeatro']."'>
+                      <a href='index.php?idTeatro=".$value['idTeatro']."&teatro=".$value['teatro']."'> 
+                        <img src='img/".$value['imagen']."' class=' img-fluid rounded' style='width:500px;height:174px;'/>
+                      </a>
                     </td>"; 
               ?>
             </tr> 
             <?php } ?>
           </tbody>
         </table>
-        </form>
+          <?php
+        }else{
+          $id_teatro=$_GET["idTeatro"];
+          $nombre_teatro=$_GET["teatro"];
+          //preparar y cegar
+          $sth = $conn->prepare("SELECT t.filas,t.columnas,t.teatro,s.fecha, s.hora,s.idSesion FROM `sesiones` as s
+          LEFT JOIN `teatros` as t
+          ON s.teatro=t.idTeatro Where s.teatro=$id_teatro");
+          $sth->execute();
+          $rs = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+          ?>
+          <h2> Sesiones <?php echo $nombre_teatro; ?> </h2>
+           <table class="table table-striped">
+          <thead>
+          <tr><th class="text-center">Fecha</th><th>Hora</th><th>Entradas disponibles</th></tr>
+          </thead>
+          <tbody>
+          <?php foreach($rs as $value){ ?>
+            <tr>
+              <?php 
+              $totalButacasTeatro=$value['filas']*$value['columnas'];
+              $idSesion=$value['idSesion'];
+              $sth1 = $conn->prepare("SELECT * FROM entradas WHERE idSesion=$idSesion");
+              $sth1->execute();
+              $rs1=$sth1->fetchAll(PDO::FETCH_ASSOC);
+             ?>
+              <td><?php echo $value['fecha']; ?> </td>
+              <td><?php echo $value['hora']; ?> </td>
+              <td><?php
+                    if ($totalButacasTeatro-count($rs1)==0){
+                      ?>
+                      <img src="img/agotadas.jpeg">
+                      <?php
+                    }else{
+                      ?>
+                      <a href="butacas.php?idTeatro=<?php echo $id_teatro; ?>&idSesion=<?php echo $idSesion; ?>">
+                        <img src="img/tickets.jpeg">
+                      </a>
+                      <?php
+                    }
+                    ?>
+                    </td> 
+              <?php }   ?>
+            </tr> 
+          </tbody>
+        </table>
+        
+         
+
+          
+
+        <?php
+        }
+        ?>
+        
+       
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
